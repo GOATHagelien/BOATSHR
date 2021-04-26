c115b4215af94d8311d4b1ff44fd0cac1cdddc79
GOATHagelien <588689@stud.hvl.no>
2021-04-26 10:15:56 +0200

    Initial commit

---
+++ src/main/java/no/hvl/dat109/BOATSHR/BoatshrApplication.java
@@ -0,0 +1,13 @@
+package no.hvl.dat109.BOATSHR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatshrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatshrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/hvl/dat109/BOATSHR/BoatshrApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat109.BOATSHR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoatshrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
